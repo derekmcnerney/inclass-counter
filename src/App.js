3c99cde4dec2c128c5cabd33d4db18bbbd40c103
derekmcnerney <derekcolin89@gmail.com>
2021-12-08 13:27:30 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import Button from "./components/Button";
 
 function App() {
+  const [number1, setNumber1] = useState(0);
+
+  const [total, setTotal] = useState(number1);
+
+  function addToTotal() {
+    setTotal(total + number1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Add the Numbers</h1>
+      <div className="number-inputs">
+        <input
+          type="number"
+          value={number1}
+          onChange={(e) => setNumber1(+e.target.value)}
+          placeholder="0"
+        />
+        
+      </div>
+      <button onClick={addToTotal}> Contribute</button>
+      <h2>{total}</h2>
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default function Button(props) {
+    let { action, title } = props;
+    return <button onClick={action}>{title}</button>;
+}
+
